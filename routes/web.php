93967002d54d89d1b85cd24523acbfa905523e58
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2025-09-28 17:35:09 +0200

    Przekazywanie zmiennych + zastosowanie szaplony template

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,12 @@ Route::get('/kontakt', function () {
     return view('kontakt');
 });
 Route::get('/onas', function () {
-    return view('onas');
+    $zadania =[
+        'Zadanie 1',
+        'Zadanie 2',
+        'Zadanie 3'
+    ];
+    //return view('onas', ['zadania' => $zadania]);
+    //return view('onas')->with('zadania', $zadania);
+    return view('onas', compact('zadania'));
 });
